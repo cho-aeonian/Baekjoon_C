3cc8f09b8672dd010f422ec310e31405c830e11d
cho-aeonian <bagchoyeong802@gmail.com>
2023-08-07 17:48:40 +0900

    baekjoon

---
+++ 10926.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+	char sentence[50];
+	scanf("%s", sentence);
+	printf("%s?\?!", sentence);
+
+	return 0;
+}
--- 10998.c
+++ 10998.c
@@ -2,7 +2,7 @@
 int main() {
 	int A, B;
 	scanf("%d %d", &A, &B);
-	printf("%d", A + B);
+	printf("%d", A * B);
 
 	return 0;
 }
